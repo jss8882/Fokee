af0093feb209a2fe3eb491fc76dd91100c29f99a
jss8882 <jss8882@unist.ac.kr>
2022-06-01 10:47:26 +0900

    before completing form

---
+++ pages/upload.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Upload: NextPage = () => {
+    return (
+        <div className={styles.container}>
+            캡슐을 업로든 하는  UI입니다
+        </div>
+    )
+}
+
+export default Upload
