b8946d7f220ac4c2474d807d2bae49c720a930e8
naanum <nanum@unist.ac.kr>
2022-06-20 09:34:02 +0900

    log in and sign up

--- README.md
+++ README.md
@@ -1,17 +1,49 @@
 ## 해야할것
 
-
 `signup` 페이지와 `login`페이지가 구현되어있어요 각각
 http://localhost:3000/signup
 http://localhost:3000/login
 에서 접근 가능합니다.
 
-여기에서(프론트엔드) 사용자가 폼을 입력하면 backend인  `api/auth.ts`에서 적적하게 반응 해주어야 합니다.
+여기에서(프론트엔드) 사용자가 폼을 입력하면 backend인 `api/auth.ts`에서 적적하게 반응 해주어야 합니다.
 
 1. `prisma/schema.prisma` 에 적절한 `User` 모델을 추가해주세요
-    - **힌트:** signup에서 입력 받는 항목들이 모델에 들어가야 겠죠??
+
+   - **힌트:** signup에서 입력 받는 항목들이 모델에 들어가야 겠죠??
+   - schema를 수정한 후에 아래 명령어를 입력하여 모델을 생성해야한다.
+
+   ```shell
+     npx prisma db push
+     #이렇게 나오면 성공입니다. Generated Prisma Client (3.14.0 | library
+   ```
+
 2. `auth.ts`를 구현하세요
    - 이 곳에서는 사용자가 login을 하려고 하는지, signup을 해야하는지에 따라서 다르게 작동해야 합니다. 해당 코드에 주석으로 설명이 되어있어요.
+
+```javascript
+// 회원가입 요청인경우 (login값이 false)
+// {
+//     email: 'jss8882@unist.ac.kr',
+//     password: '12341234',
+//     passwordCheck: '12341234',
+//     firstName: '장',
+//     lastName: '상수',
+//     position: '학생',
+//     login: false
+//   }
+////비밀번호와 비밀번호 확인이 같은지 비교한다
+//////틀리면 바로 return
+//////같으면 아래를 수행
+////////Prisma에 새로운 USER를 추가한다.
+
+//로그인 요청인 경우(login 값이 true)
+// { email: 'jss8882@unist.ac.kr', password: '12341234', login: true }
+////해당 이메일 가진 유저가 데이터베이스에 있는지 체크
+/////없으면 return res.json({ok:false, error:"no user please signup first"})
+/////있으면 비밀번호 가 동일한지 체크
+//////동일하면 return res.json({ok:true,error:"we are now logged in"})
+//////비밀번호가 다르면 return res.json({ok:false, error:"wrong password"})
+```
 
 회원 가입시에는 새로운 `User`를 `create`하면 됩니다 해당 내용은 아래 링크에 있어요
 https://www.prisma.io/docs/concepts/components/prisma-client/crud#create
--- pages/api/auth.ts
+++ pages/api/auth.ts
@@ -1,58 +1,86 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-import client from '../../prisma/client'
-
+import { prisma } from "@prisma/client";
+import type { NextApiRequest, NextApiResponse } from "next";
+import client from "../../prisma/client";
 
 interface SignupRequestType {
-    email:string;
-    password:string;
-    passwordCheck:string;
-    firstName:string;
-    lastName:string;
-    position:string;
-    login:boolean
+	email: string;
+	password: string;
+	passwordCheck: string;
+	firstName: string;
+	lastName: string;
+	position: string;
+	login: boolean;
 }
 
 interface LoginRequestType {
-    email:string;
-    password:string;
-    login:boolean
+	email: string;
+	password: string;
+	login: boolean;
 }
 
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+	console.log(req.body);
+	const { login } = req.body;
+	// const login = req.body.login
+	console.log(login);
+	if (login) {
+		// const foundCapsule = await client.capsule.findUnique({
+		//     where:{
+		//         id: 5
+		//     }
+		// })
+		const userInfo: LoginRequestType = req.body;
+		const userCheck = await client.user.findUnique({
+			where: {
+				email: userInfo.email,
+			},
+		});
+		if (userCheck) {
+			if (userInfo.password === userCheck.password) {
+				console.log("성공~");
+				console.log(userCheck);
+				return res.json({ ok: true, message: "we are now logged in", user: { ...userCheck } });
+			} else {
+				console.log("비밀번호틀렷따");
+				return res.json({ ok: false, error: "wrong password" });
+			}
+		} else {
+			console.log("아이디업따");
+			return res.json({ ok: false, error: "no user; please sign up first" });
+		}
+	} else {
+		// const foundCapsule = await client.capsule.findUnique({
+		//     where:{
+		//         id: 5
+		//     }
+		// })
 
+		const newUserInfo: SignupRequestType = req.body;
+		const userCheck = await client.user.findUnique({
+			where: {
+				email: newUserInfo.email,
+			},
+		});
+		console.log(userCheck);
+		if (userCheck) {
+			return res.json({ ok: false, error: "you already have signed up" });
+		} else if (newUserInfo.password === newUserInfo.passwordCheck) {
+			//console.log("wow");
+			const newUser = await client.user.create({
+				data: {
+					email: newUserInfo.email,
+					password: newUserInfo.password,
+					firstName: newUserInfo.firstName,
+					lastName: newUserInfo.lastName,
+					position: newUserInfo.position,
+				},
+			});
+			return res.json({ ok: true, message: "성공!!" });
+		} else {
+			return res.json({ ok: false, error: "bimilbunhoga darda" });
+		}
+	}
 
-export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse
-) {
-
-    console.log(req.body)
-
-    // 회원가입 요청인경우 (login값이 false)
-    // {
-    //     email: 'jss8882@unist.ac.kr',
-    //     password: '12341234',
-    //     passwordCheck: '12341234',
-    //     firstName: '장',
-    //     lastName: '상수',
-    //     position: '학생',
-    //     login: false
-    //   }
-    ////비밀번호와 비밀번호 확인이 같은지 비교한다
-    //////틀리면 바로 return
-    //////같으면 아래를 수행
-    ////////Prisma에 새로운 USER를 추가한다.
-    
-    //로그인 요청인 경우(login 값이 true)
-    // { email: 'jss8882@unist.ac.kr', password: '12341234', login: true }
-    ////해당 이메일 가진 유저가 데이터베이스에 있는지 체크
-    /////없으면 return res.json({ok:false, error:"no user please signup first"})
-    /////있으면 비밀번호 가 동일한지 체크
-    //////동일하면 return res.json({ok:true,error:"we are now logged in"})
-    //////비밀번호가 다르면 return res.json({ok:false, error:"wrong password"})
-
-
-
-
-    res.status(200).json({ name: 'John Doe' })
+	// res.status(200).json({ name: "John Doe" });
 }
